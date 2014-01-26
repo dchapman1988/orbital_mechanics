f587acfec520e517269ebf1954969ec8507b70ec
David Chapman <dchapman1988@gmail.com>
2014-01-25 21:20:39 -0600

    Documenting OribitalMechanics::Constants module

--- lib/orbital_mechanics/constants.rb
+++ lib/orbital_mechanics/constants.rb
@@ -1,4 +1,6 @@
 module OrbitalMechanics
+  # The Constants module for convienently including
+  # the physical constants needed in orbital mechanics
   module Constants
     # Gravitational Constant
     G = 6.67384 * 10**-11 # m3/(kg·s2)
